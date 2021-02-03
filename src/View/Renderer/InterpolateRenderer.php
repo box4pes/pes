a22c9319176e5cb04908586393641cefeffd5525
Petr Svoboda <svoboda@grafia.cz>
2021-02-03 17:12:47 +0100

    renderer containe exception

--- src/View/Renderer/InterpolateRenderer.php
+++ src/View/Renderer/InterpolateRenderer.php
@@ -22,8 +22,7 @@ use Pes\View\Renderer\Exception\UnsupportedTemplateException;
 class InterpolateRenderer implements InterpolateRendererInterface {
 
     private $template;
-
-
+ 
     public function setTemplate(FileTemplateInterface $template) {
         if ($template->getDefaultRendererService() !== InterpolateRenderer::class) {
             throw new UnsupportedTemplateException("Renderer ". get_called_class()." nepodporuje renderování template typu ". get_class($this->template));
