5755add63a1e296d2f2037174297bfd214b898a9
Petr Svoboda <svoboda@grafia.cz>
2021-02-05 18:09:44 +0100

    UriInfoFactory fix

--- src/Application/UriInfoFactory.php
+++ src/Application/UriInfoFactory.php
@@ -48,7 +48,7 @@ class UriInfoFactory implements UriInfoFactoryInterface {
         // objekt UrlInfo atribut s názvem self::URL_INFO_ATTRIBUTE_NAME do requestu a request do app
         $urlInfo = new UrlInfo();
         $urlInfo->setSubdomainUri($subDomainPath);
-        $urlInfo->setSubdomainPath('/'. $this->normalizePath($requestScriptDir));
+        $urlInfo->setSubdomainPath($this->normalizePath($requestScriptDir));
         $urlInfo->setRestUri($virtualPath);
         $urlInfo->setRootAbsolutePath($this->rootAbsolutePath($environment));
         $urlInfo->setWorkingPath($this->workingPath());
