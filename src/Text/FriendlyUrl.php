b6670eab17cda6c4156a597cb88d4c52086534d8
grafiaware <grafiaware@grafia.cz>
2022-01-02 16:10:09 +0100

    add FriendlyUrl to Text

---
+++ src/Text/FriendlyUrl.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Pes\Text;
+
+/**
+ * Description of FriendlyUrl
+ *
+ * @author pes2704
+ */
+class FriendlyUrl implements FriendlyUrlInterface {
+    public function friendlyUrlText($sourceText) {
+        $url = $sourceText;
+        $url = preg_replace('~[^\\pL0-9_]+~u', '-', $url);
+        $url = trim($url, "-");
+        $url = iconv("utf-8", "us-ascii//TRANSLIT", $url);
+        $url = strtolower($url);
+        $url = preg_replace('~[^-a-z0-9_]+~', '', $url);
+        return $url;
+    }
+}
---
+++ src/Text/FriendlyUrlInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Pes\Text;
+
+/**
+ *
+ * @author pes2704
+ */
+interface FriendlyUrlInterface {
+    public function friendlyUrlText($sourceText);
+}
