f6cc8a7d4ce0da5ac58bae3965e622cd6e8cb3da
grafiaware <grafiaware@grafia.cz>
2022-12-10 21:54:48 +0100

    Text - asc, esc_js. esc_Url - add & fix

--- src/Text/TextInterface.php
+++ src/Text/TextInterface.php
@@ -31,32 +31,7 @@ interface TextInterface {
      * @param type $text
      * @return type
      */
-    public static function filter($filters, $text);
-
-    /**
-     * Alias k metodě esc().
-     *
-     * @param type $text
-     * @return type
-     */
-    public static function e($text);
-
-    /**
-     * Metodu použijte na ošetření textů, které obsahují text zadaný uživatelem (např. ve formuláři). Jde o základní ochranu proti XSS útokům.
-     * Metoda provede tzv escapování. Všechny znaky, které mohou v HTML mít význam, tzv. rezervované znaky HTML, převede na HTML entity.
-     * Např. znak < na &lt; apod.
-     * Metoda nestačí na ošetření textů vkládaných kamkoli jinam než jen do textového obsahu tagu určeného k zobrazení.
-     * Nikdy nevkládejte uživatelem zadaný text do obsahu tagu <script>, do html komentáře <!-- -->, do názvu atributu, do jména tagu, do css.
-     * Tato místa nelze nikdy dokonale ošetřit.
-     *
-     * Tato metoda escapuje i html entity, které byly v opravovaném textu, např. pokud text obsahuje "mluví o&nbsp;všem" vznikne "mluví o&amp;nbsp;všem".
-     * pozor tedy také na pořadí filtrovacích metod: $this->filter('e|mono', 'v neděli'); je v pořádku, zatímco $this->filter('mono|e', 'v neděli');
-     * oescapuje i &nbsp; vytvořené filtrem "mono".
-     *
-     * @param type $text
-     * @return type
-     */
-    public static function esc($text);
+    public static function filter(string $filters, string $text): string;
 
     /**
      * Jednoznakové předložky a spojky pro český text. Metoda vloží mezi jednoznakové předložky nebo spojky a následující slovo nedělitelnou mezeru.
@@ -65,22 +40,30 @@ interface TextInterface {
      * @param type $text
      * @return type
      */
-    public static function mono($text);
+    public static function mono(string $text): string;
+
+    /**
+     * Řetěcec datumu v českém formátu normalizuje na tvar den. měsíc. rok, čísla bez levostraných nul a s tečkou (pořadová) a mezi tečkou a následujícím číslem obyčejná mezera.
+     *
+     * @param string $dateCsFormatted
+     */
+    public static function dateCsSpaces(string $dateCsFormatted): string;
+
+    /**
+     * Řetěcec datumu v českém formátu normalizuje na tvar den. měsíc. rok, čísla bez levostraných nul a s tečkou (pořadová) a mezi tečkou a následujícím číslem
+     * nerozděkitelná HTML mezera - řetězec &nbsp;
+     *
+     * @param string $dateCsFormatted
+     */
+    public static function dateCsNbsp(string $dateCsFormatted): string;
 
     /**
      * Převede odřádkování v textu na značku (tag) <br />. Převádí každé odřádkování, vícenásobné odřádkování způsobí vícenásobné vložení značky.
      *
      * @param type $text
-     * @return type
+     * @return string
      */
-    public static function nl2br($text);
-
-    /**
-     * Escapuje retězec, který obsahuje javascript, který má být vložen jako inline javarcript do atributu onclick, onblur atd.
-     *
-     * @param string $text
-     */
-    public static function esc_js($text='');
+    public static function nl2br(string $text): string;
 
     /**
      *
@@ -89,5 +72,31 @@ interface TextInterface {
      * @param string $textOnFalse
      * @return string
      */
-    public static function resolve($condition = false, $textOnTrue = '', $textOnFalse = '');
+    public static function resolve($condition = true, string $textOnTrue, string $textOnFalse = ''): string;
+
+    /**
+     * Alias k metodě esc().
+     *
+     * @param string $text
+     * @return type
+     */
+    public static function e(string $text): string;
+
+    /**
+     * Metoda musí nahrazovat znaky způsobem vhodným pro použízí výsledného textu v HTML5.
+     *
+     * @param string $text Text k escapování
+     */
+    public static function esc(string $text): string;
+
+    public static function esc_attr(string $text): string;
+
+    /**
+     * Escapuje retězec, který obsahuje javascript, který má být vložen jako inline javarcript do atributu onclick, onblur atd.
+     *
+     * @param string $text
+     */
+    public static function esc_js(string $text): string;
+    
+    public static function esc_Url(string $string): string;
 }
