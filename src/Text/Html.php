dffa6da943de91e0386549b541a444ed83e47b5b
pes2704 <pes2704@grafia.cz>
2024-10-18 16:21:40 +0200

    Htm - attributes - urušeno vynechání atrinutů s "prázdnými" hodnotami (např.prázdný řetězec - value='')

--- src/Text/Html.php
+++ src/Text/Html.php
@@ -71,14 +71,10 @@ class Html implements HtmlInterface {
                 }
             } elseif (is_array($value)) {
                 $attrVal = implode(' ', array_filter($value, function($value) {return boolval($value);}));  // filtruje prázdné řetězce
-//                if ($attrVal) {
                     $attr[] = $type.'="'.$attrVal.'"';
-//                }
             } else {
                 $attrVal = trim((string) $value);
-//                if ($attrVal) {
                     $attr[] = $type.'="'.$attrVal.'"';
-//                }
             }
         }
         return isset($attr) ? implode(' ',$attr) : '';
