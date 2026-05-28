01f7537c2790f361e8bf6fc90d73b405c1ce3bf3
box4pes <pesbox@email.cz>
2026-05-28 15:23:39 +0200

    fix shoda s interface ResourceInterface

--- pes-router/src/Resource/ResourceInterface.php
+++ pes-router/src/Resource/ResourceInterface.php
@@ -13,17 +13,32 @@ namespace Pes\Router\Resource;
  * @author pes2704
  */
 interface ResourceInterface {
-    
+    /**
+     * @param string $httpMethod
+     * @return ResourceInterface
+     */
     public function withHttpMethod(string $httpMethod): ResourceInterface;
 
+    /**
+     * @param string $urlPattern
+     * @return ResourceInterface
+     */
     public function withUrlPattern(string $urlPattern): ResourceInterface;
 
+    /**
+     * @return string
+     */
     public function getHttpMethod(): string;
 
+    /**
+     * @return string
+     */
     public function getUrlPattern(): string;
 
     /**
+     * @param array $params
      * @return string Path se zadanými parametry.
+     * @throws ResourcePathParameterDoesNotMatch
      */
     public function getPathFor(array $params): string;
 }
